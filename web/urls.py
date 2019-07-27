7945ba8cdff80ceb298c880c606b23b14bb1a25c
songjh78 <fredmail03@126.com>
2019-07-27 17:25:46 +0800

    customer_list_web_urls.py: name

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,5 @@ from django.urls import re_path, path
 from web.views import customer, payment
 
 urlpatterns = [
-    re_path(r'customer/list/$', customer)
+    re_path(r'customer/list/$', customer.customer_list, name='customer_list')
 ]
